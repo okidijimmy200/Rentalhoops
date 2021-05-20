12b852f407b58ad078774604720d9557dcde4355
Ssejjemba Dan <ssejjembadan@gmail.com>
2021-05-20 09:56:27 +0300

    Improve properties to remove bad code

--- client/src/components/SliderData.js
+++ client/src/components/SliderData.js
@@ -1,14 +1,26 @@
 export const SliderData = [
-    { 
-        image:
-        '/api/property/propertyphotoprimary/'
-    },
-    {
-        image: 
-        '/api/property/propertyphotosecondary/'
-    },
-    {
-        image:
-        '/api/property/propertyphototetiary/'
-    }
+  {
+    image: "/api/property/propertyphotoprimary/",
+  },
+  {
+    image: "/api/property/propertyphotosecondary/",
+  },
+  {
+    image: "/api/property/propertyphototetiary/",
+  },
+];
+
+export const propertyImages = (property_id) => [
+  {
+    id: "propertyphotoprimary",
+    url: `/api/property/propertyphotoprimary/${property_id}`,
+  },
+  {
+    id: "propertyphotosecondary",
+    url: `/api/property/propertyphotosecondary/${property_id}`,
+  },
+  {
+    id: "propertyphototetiary",
+    url: `/api/property/propertyphototetiary/${property_id}`,
+  },
 ];
--- client/src/property/LandlordProperty.js
+++ client/src/property/LandlordProperty.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 import auth from "./../auth/auth-helper";
@@ -7,16 +6,15 @@ import { listByLandlord } from "./../property/api-property";
 import { Redirect } from "react-router-dom";
 import Grid from "@material-ui/core/Grid";
 import { useStyles } from "./styles";
+import Property from "./Property";
 
 // styling the component
 
 export default function LandlordProperty({ slides }) {
   const classes = useStyles();
-  const [property, setProperty] = useState([]);
+  const [properties, setProperties] = useState([]);
   const [redirectToSignin, setRedirectToSignin] = useState(false);
   const jwt = auth.isAuthenticated();
-  const [current, setCurrent] = useState(0);
-  const length = slides.length;
   //fetch the list property API
   useEffect(() => {
     const abortController = new AbortController();
@@ -31,7 +29,7 @@ export default function LandlordProperty({ slides }) {
       if (data.error) {
         setRedirectToSignin(true);
       } else {
-        setProperty(data);
+        setProperties(data);
       }
     });
     return function cleanup() {
@@ -39,15 +37,6 @@ export default function LandlordProperty({ slides }) {
     };
     // eslint-disable-next-line
   }, [jwt.user._id]);
-
-  const nextSlide = () => {
-    //  console.log('event.currentTarget.dataset.id', event.currentTarget.dataset.id);
-    setCurrent(current === length - 1 ? 0 : current + 1);
-  };
-
-  const prevSlide = () => {
-    setCurrent(current === 0 ? length - 1 : current - 1);
-  };
 
   if (!Array.isArray(slides) || slides.length <= 0) {
     return null;
@@ -61,8 +50,12 @@ export default function LandlordProperty({ slides }) {
       <Paper style={{ boxShadow: "0 0 0 0" }}>
         <Typography>Your Properties</Typography>
         <Grid container spacing={4} className={classes.gridRoot}>
-          {property.map((property, i) => {
-            return <Grid item xs={12} sm={12} md={6} key={i}></Grid>;
+          {properties.map((property, i) => {
+            return (
+              <Grid item xs={12} sm={12} md={6} key={i}>
+                <Property property={property} />
+              </Grid>
+            );
           })}
         </Grid>
       </Paper>
