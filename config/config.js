c268fd883436868a0fd1c1a89b2d83ac56baace9
okidijimmy200 <okidijimmie@gmail.com>
2021-08-19 15:03:01 +0300

    made changes to files

--- config/config.js
+++ config/config.js
@@ -3,14 +3,14 @@ const dotenv = require('dotenv')
 dotenv.config({path: './.env'})
 
 // const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
-const LocalDB = process.env.MONGO_URI
+const LocalDB = process.env.DATABASE_LOCAL
 
 console.log(LocalDB)
 
 
 const config = {
   env: process.env.NODE_ENV || 'development',
-  port: process.env.PORT || 8080,
+  port: process.env.PORT || 9999,
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
   mongoUri: LocalDB,
 }
